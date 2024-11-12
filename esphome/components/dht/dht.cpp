5a119b2d68e9dbd427e7d63ca6e7da17b1367fa6
Vidmantas Jurka <89202006+vjurka@users.noreply.github.com>
2024-11-12 14:31:48 +0200

    Update dht.cpp

--- esphome/components/dht/dht.cpp
+++ esphome/components/dht/dht.cpp
@@ -88,12 +88,11 @@ bool HOT IRAM_ATTR DHT::read_sensor_(float *temperature, float *humidity, bool r
   } else if (this->model_ == DHT_MODEL_SI7021) {
 #ifdef USE_ESP8266
     delayMicroseconds(500);
-    this->pin_->digital_write(true);
-    delayMicroseconds(40);
 #else
     delayMicroseconds(400);
-    this->pin_->digital_write(true);
 #endif
+    this->pin_->digital_write(true);
+    delayMicroseconds(40);
   } else if (this->model_ == DHT_MODEL_DHT22_TYPE2) {
     delayMicroseconds(2000);
   } else if (this->model_ == DHT_MODEL_AM2120 || this->model_ == DHT_MODEL_AM2302) {
@@ -108,7 +107,7 @@ bool HOT IRAM_ATTR DHT::read_sensor_(float *temperature, float *humidity, bool r
     // Host pull up 20-40us then DHT response 80us
     // Start waiting for initial rising edge at the center when we
     // expect the DHT response (30us+40us)
-    delayMicroseconds(70);
+    delayMicroseconds(40);
 
     uint8_t bit = 7;
     uint8_t byte = 0;

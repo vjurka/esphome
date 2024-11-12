af1b3b5f52a8663a08a5c24d6b9dbb78c9cf891f
Vidmantas Jurka <89202006+vjurka@users.noreply.github.com>
2024-11-12 15:05:16 +0200

    Update dht.cpp

--- esphome/components/dht/dht.cpp
+++ esphome/components/dht/dht.cpp
@@ -107,7 +107,11 @@ bool HOT IRAM_ATTR DHT::read_sensor_(float *temperature, float *humidity, bool r
     // Host pull up 20-40us then DHT response 80us
     // Start waiting for initial rising edge at the center when we
     // expect the DHT response (30us+40us)
-    delayMicroseconds(45);
+#ifdef USE_ESP8266
+    delayMicroseconds(70);
+#else
+    delayMicroseconds(30);
+#endif
 
     uint8_t bit = 7;
     uint8_t byte = 0;
